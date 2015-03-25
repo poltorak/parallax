adfa42a15f48cbe6e1bb931c67c885c02baebc75
Szymon Półorak <poltorak.szymon@gmail.com>
2015-03-25 14:51:02 +0100

    Add offset

--- jquery.parallax.js
+++ jquery.parallax.js
@@ -19,11 +19,12 @@ http://www.gnu.org/licenses/gpl.html
 		windowHeight = $window.height();
 	});
 
-	$.fn.parallax = function(xpos, speedFactor, outerHeight) {
+	$.fn.parallax = function(xpos, speedFactor, offset, outerHeight) {
 		var $this = $(this);
 		var getHeight;
 		var firstTop;
 		var paddingTop = 0;
+		
 		
 		//get the starting position of each element to have parallax applied to it		
 		$this.each(function(){
@@ -43,7 +44,8 @@ http://www.gnu.org/licenses/gpl.html
 		// setup defaults if arguments aren't specified
 		if (arguments.length < 1 || xpos === null) xpos = "50%";
 		if (arguments.length < 2 || speedFactor === null) speedFactor = 0.1;
-		if (arguments.length < 3 || outerHeight === null) outerHeight = true;
+		if (arguments.length < 3 || offset === null) offset = 0;
+		if (arguments.length < 4 || outerHeight === null) outerHeight = true;
 		
 		// function to be called whenever the window is scrolled or resized
 		function update(){
@@ -59,7 +61,7 @@ http://www.gnu.org/licenses/gpl.html
 					return;
 				}
 
-				$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
+				$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos + offset) * speedFactor) + "px");
 			});
 		}		
 
